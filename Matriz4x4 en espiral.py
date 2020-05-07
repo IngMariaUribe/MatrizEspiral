fdf26e908846bba9968491001ec4ac8723d52014
IngMariaUribe <51063535+IngMariaUribe@users.noreply.github.com>
2020-05-07 12:43:15 -0500

    Update Matriz4x4 en espiral.py

--- Matriz4x4 en espiral.py
+++ Matriz4x4 en espiral.py
@@ -6,41 +6,50 @@ if __name__ == "__main__":
      
     num=4
     value=0
+    aux=[]
    
-matriz = [[0, 0, 0, 0],
-          [0, 0, 0, 0],
-          [0, 0, 0, 0],
-          [0, 0, 0, 0]]
+matriz = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16]]
+print "Matriz inicial"
+for a in range (num):
+    print matriz[a]\n 
+    
+for i in range (num):
+    for j in range (num):
+        aux.append(matriz[i][j])
+    
 
 vuelta=num/2
 for k in range (vuelta):
     for j in range(num-2*k):
         fila=0+k
         columna=j+k
+        matriz[fila][columna] = aux[value]
         value = value+1
-        matriz[fila][columna] = value
         
     if k!=vuelta-1:            
         for i in range(num-1-k):
             fila=i+1
             columna=num-1
+            matriz[fila][columna] = aux[value]
             value = value+1
-            matriz[fila][columna] = value
                 
     for j in range(num-1-k):
         fila=num-1-k
         columna=num-2-j
+        matriz[fila][columna] = aux[value]
         value = value+1
-        matriz[fila][columna] = value
 
     if k!=vuelta-1:
      for i in range(num-2):
         fila=num-2-i
         columna=0
+        matriz[fila][columna] = aux[value]
         value = value+1
-        matriz[fila][columna] = value
         
-
+print "Matriz Final"
 for a in range (num):
     print matriz[a]\n    
      
